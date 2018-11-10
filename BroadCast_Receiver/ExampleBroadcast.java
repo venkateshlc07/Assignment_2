1a06e8322b44c7aca6a58e16b8da71f9bda96cc5
venkateshlc07 <42529479+venkateshlc07@users.noreply.github.com>
2018-11-10 10:18:45 +0530

    Add files via upload

---
+++ BroadCast_Receiver/ExampleBroadcast.java
@@ -0,0 +1,20 @@
+package com.example.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBrodcast extends BroadcastReceiver {
+	
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+			Toast.makeText(context,"Connectivity Changed", Toast.LENGTH_SHORT).show();
+		}
+		
+	}
+
+}
